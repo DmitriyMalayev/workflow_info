fd2aa1cbddf15254e1e8c7ead491dce7ef960999
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-10-05 04:03:32 -0400

    production and development

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist/assets"),
     filename: "./bundle.js",
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    publicPath: "/assets/",
+  },
 };
 
 // We are exporting a web configuration object. Takes two properties, an entry property and an output property.
@@ -19,11 +23,11 @@ module.exports = {
 // Running Webpack Requirements
 // Webpack Core and Webpack CLI
 // npm install webpack webpack-cli --save-dev
-// node_modules/.bin/webpack 
+// node_modules/.bin/webpack
 // After adding it to package.json we can do   npm run webpack
 
 // Module
-// A chunk of code that we can export from one file and import in another. 
+// A chunk of code that we can export from one file and import in another.
 
 // Export
-// If we want to use the files we need to prefix our methods with export and import them in the file needed. 
+// If we want to use the files we need to prefix our methods with export and import them in the file needed.
--- webpack.md
+++ webpack.md
@@ -3,3 +3,15 @@ Webpack is a popular module bundler and front end tool.
 It lets us bundle multiple files into one file such as bundle.js
 Works well with babel
 Ability to start local development server
+
+# Webpack Dev Server Installation
+npm install webpack-dev-server
+npm run serve 
+
+
+```json
+  "scripts": {
+    "build": "node_modules/.bin/webpack --mode production",      //production and building. Slower but optimized. 
+    "serve": "webpack-dev-server"     //development. faster. 
+  }
+```
